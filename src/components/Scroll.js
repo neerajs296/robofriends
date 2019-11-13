0c43ab94d7ac72252a1c98377113632a7351e86b
Neeraj Sharma <Neeraj.Sharma5@gds.ey.com>
2019-11-13 23:37:45 +0530

    Adding Border

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflow: 'scroll', border: '10px solid black', height: '800px'}}>
+    <div style={{ overflow: 'scroll', border: '8px solid black', height: '800px'}}>
       {props.children}
     </div>
   );
